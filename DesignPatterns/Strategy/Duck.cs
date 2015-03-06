c901f142cf00d94f0c8872e6ad489d351f2d8d76
Brad Rosen <nywebman2@yahoo.com>
2015-03-06 15:52:22 -0500

    Strategy Pattern
    (Behavioral)

---
+++ DesignPatterns/Strategy/Duck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Strategy
+{
+    public class Duck
+    {
+        private IQuackBehavior QuackStrategy { get; set; }
+        public Duck(IQuackBehavior q)
+        {
+            QuackStrategy = q;
+        }
+
+        public void Quack()
+        {
+            QuackStrategy.Quack();
+        }
+    }
+
+
+}
