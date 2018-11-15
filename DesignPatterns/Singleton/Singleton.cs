99c1d2a90d6d51109da5c0225990d35ecea6ab6a
Brad Rosen <brosen@mdsol.com>
2018-11-15 12:46:39 -0500

    Singleton example 2

---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
+
+
+/*
+ usage 
+ Singleton s1 = Singleton.Instance();
+ Singleton s2 = Singleton.Instance(); -> same as s1
+
+*/
